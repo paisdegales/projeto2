3635a226bbecfd6f7410d2d67f897531c9ff87d6
paisdegales <b.lessa@escola.pr.gov.br>
2025-06-04 14:34:26 +0000

    projeto completo

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+
+criaCartao(
+    'Historia',
+    'Quem descobriu a America?',
+    'Cristóvão Colombo'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a menor cidade do mundo?',
+    'Hum, na Croácia'
+)
+
+criaCartao(
+    'Português?',
+    'Qual a maior palavra da Língua portuguêsa?',
+    'pneumoultramicroscopicossilicovulcanoconiótico'
+)
+
+criaCartao(
+    'Esporte',
+    'Qual o esporte mais praticado no mundo?',
+    'futebol'
+)
